6819d9b6a802d0cd53b82b75d661e484a9adcb17
longbui98 <quanglongbui153@gmail.com>
2022-12-30 14:23:05 +0700

    Test squash

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const bodyParse = require("body-parser");
-const path = require('path');
-const date = require(path.resolve(__dirname + "/date.js"));
+const date = require(__dirname + "/date.js");
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParse.urlencoded({ extends: true }));
+const path = require('path');
 
+// set the view engine to ejs
+app.set('ciews', path.resolve() + '/views');
+app.set('view engine', 'ejs');
+
+app.use(bodyParse.urlencoded({ extends: true }));
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
 let itemWorks = [];
@@ -15,14 +19,15 @@ let itemWorks = [];
 app.get("/", function (req, res) {
   let day = date.getDay();
 
-  res.render(path.resolve(__dirname + "/views/list.ejs"), { listTitle: day, listItem: items });
+  res.render("list", { listTitle: day, listItem: items });
   items.pop;
 });
 
 app.post("/", function (req, res) {
-  let item = req.body.foodservice;
 
+  let item = req.body.foodservice;
   if(res.body.list === "work"){
+    console.log("-----work------");
     itemWorks.push(item);
     res.redirect("/work");    
   }else{
@@ -35,9 +40,12 @@ app.post("/", function (req, res) {
 });
 
 app.get("/work", function (req, res) {
-  res.render(path.resolve(__dirname + "/views/list.ejs"), { listTitle: "Work List", listItem: itemWorks });
+  res.render("list", { listTitle: "Work List", listItem: itemWorks });
 });
 
 app.listen(port, function (req, res) {
   console.log(`Server is running on ${port}`);
 });
+
+
+///asdasdasd
